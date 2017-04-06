5abf8768edff518aec4628a61ed07f1112e67b81
Caesar1874 <iwangqinghe@gmail.com>
2017-04-06 21:20:26 +0800

    interview

---
+++ ES/变量声明提升.js
@@ -0,0 +1,13 @@
+
+(function(condition) {
+        if( condition) {
+            var x = 3;
+            console.log(x)
+        } else {
+            var y = 4;
+            console.log(y);
+        }
+        console.log(x)
+        console.log(y)
+    })
+(new Date().getTime() % 2)
