1d918b0d25b2ec52e99df14f8484a55a2f8b4d4b
qinghe <qinghe@2dfire.com>
2017-06-03 10:29:12 +0800

    合并 panning  code

--- 高级/自定义事件/DragDrop/drag.js
+++ 高级/自定义事件/DragDrop/drag.js
@@ -1,13 +1,15 @@
 
 const followPointer = (target, event)=> {
-    console.log('fo',target)
+    // console.log('fo',target)
     const x = event.clientX
     const y = event.clientY
-    console.log(x, y)
+    // console.log(x, y)
     target.style.left = x + 'px'
     target.style.top = y + 'px'
 }
 
+var undo = []
+const draggable = document.querySelector('.draggable')
 class DragDrop {
     constructor() {
         this.target = null
@@ -15,35 +17,39 @@ class DragDrop {
     }
     enable() {
         const handleEvent = this.handleEvent
-        document.addEventListener('mousedown', handleEvent)
-        document.addEventListener('mousemove', handleEvent)
-        document.addEventListener('mouseup', handleEvent)
+        draggable.addEventListener('mousedown', handleEvent)
+        draggable.addEventListener('mousemove', handleEvent)
+        draggable.addEventListener('mouseup', handleEvent)
     }
     disable() {
         const handleEvent = this.handleEvent
-        document.removeEventListener('mousedown', handleEvent)
-        document.removeEventListener('mousemove', handleEvent)
-        document.removeEventListener('mouseup', handleEvent)
+        draggable.removeEventListener('mousedown', handleEvent)
+        draggable.removeEventListener('mousemove', handleEvent)
+        draggable.removeEventListener('mouseup', handleEvent)
     }
 
 
     handleEvent(event) {
         const target = event.target;
         const type = event.type
-        console.log('this', this)
+        // console.log('this', this)
         if(type === 'mousedown') {
-            console.log('mousedown')
+            // console.log('mousedown')
             if(target.classList.contains('draggable')) {
-                console.log('this.target', this.target)
+                // console.log('this.target', this.target)
                 this.target = target
             }
         } else if(type === 'mousemove') {
             if(this.target !== null) {
-                console.log('mosemove')
+                // console.log('mosemove')
+                // 当前坐标
+                const x = event.clientX
+                const y = event.clientY
+                undo.push({x, y})
                 followPointer(this.target, event)
             }
         } else if(type === 'mouseup') {
-            console.log('mouseup')
+            // console.log('mouseup')
             this.target = null
         }
     }
@@ -61,6 +67,16 @@ const dragDrop = (() => {
 
 const inst = dragDrop()
 inst.enable()
+
+const btn = document.querySelector("#undo")
+
+btn.addEventListener('click', function(event) {
+    console.log('click')
+    var {x, y} = undo.pop()
+    console.log(x, y)
+    draggable.style.left = x + 'px'
+    draggable.style.top = y + 'px'
+})
 
 
 
