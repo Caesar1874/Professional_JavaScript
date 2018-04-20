f0153427b5d2f67376a8b4efe2a1d66fbe0e91be
caesarii <iwangqinghe@gmail.com>
2018-04-20 10:17:41 +0800

    安全类型检测

--- 高级话题/类型检测/安全的类型检测.js
+++ 高级话题/类型检测/安全的类型检测.js
@@ -1,10 +1,12 @@
 
+// 安全类型检测的原理是 在任何值上调用 Object.prototype.toString() 方法会返回 [object NativeContstructorName] 格式的字符串
+// 该方法对于非原生类型始终返回 [object Object] 所以可以区分原生类型与非原生类型
 
-
-const testInstanceOf = () => {
-    const w1 = new window()
-    const w2 = new window()
+const isArray = (array) => {
+    return Object.prototype.toString.call(array) === '[object Array]'
 }
 
+const isFunction = (func) => {
+    return Object.prototype.toString.call(func) === '[object Function]'
+}
 
-testInstanceOf()
